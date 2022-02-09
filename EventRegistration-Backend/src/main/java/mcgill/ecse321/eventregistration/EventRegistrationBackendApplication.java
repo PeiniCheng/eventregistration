26e8726df32f3d38c4f14ff07bc11250a56822ae
PeiniCheng <peini.cheng@mail.mcgill.ca>
2022-02-09 13:46:58 -0500

    Update EventRegistrationBackendApplication.java

--- EventRegistration-Backend/src/main/java/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
+++ EventRegistration-Backend/src/main/java/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
@@ -2,6 +2,7 @@ package mcgill.ecse321.eventregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class EventRegistrationBackendApplication {
